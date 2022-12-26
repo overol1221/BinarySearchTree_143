f9af57a7eb74af805311f91b29ee8f211a9c7d20
overol1221 <fiqiachmad12@gmail.com>
2022-12-26 17:18:26 +0700

    Create class node for l and r child

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    internal class Program
+    class node
     {
+        public string info;
+        public node lchild;
+        public node rchild;
+
+        //Constructor for the node class
+
+
+
+
+
+
         static void Main(string[] args)
         {
         }
